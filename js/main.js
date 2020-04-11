2ad20d9cf21aedb254841aa7cb2363db079b5b4e
oumaymajaouadi <60550313+oumaymajaouadi@users.noreply.github.com>
2020-04-11 17:21:28 +0100

    javascript checkpoints

--- js/main.js
+++ js/main.js
@@ -37,3 +37,42 @@ var textArea = document.getElementById("para") ;
 textArea.style.fontFamily = value;
 
 }
+$(".img1").hover(function(){
+    $(".btn1").toggleClass('btnHover')
+})
+
+$(".img2").hover(function(){
+    $(".btn2").toggleClass('btnHover')
+})
+
+$(".img3").hover(function(){
+    $(".btn3").toggleClass('btnHover')
+})
+$(".img4").hover(function(){
+    $(".btn4").toggleClass('btnHover')
+})
+
+$(".img5").hover(function(){
+    $(".btn5").toggleClass('btnHover')
+})
+
+$(".img6").hover(function(){
+    $(".btn6").toggleClass('btnHover')
+})
+$(".img7").hover(function(){
+    $(".btn7").toggleClass('btnHover')
+})
+
+$(".img8").hover(function(){
+    $(".btn8").toggleClass('btnHover')
+})
+
+$(".img9").hover(function(){
+    $(".btn9").toggleClass('btnHover')
+})
+
+$(".courses > div > button").click(function(e){
+    $("#ex1").modal('open')
+})
+
+
